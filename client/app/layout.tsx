426c651b6ad31a7b82e4de487412887ec32eee53
Luis <luis.luis@icloud.com>
2024-11-17 15:06:21 -0600

    add styles

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/providers";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,9 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.className}  antialiased`}>
         <Providers>{children}</Providers>
       </body>
     </html>
