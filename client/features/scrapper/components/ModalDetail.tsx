456b8ce17bc6bda4362cc2c002bcd969ff12746c
Luis <luis.luis@icloud.com>
2024-11-27 08:56:10 -0600

    add web proxy

--- client/features/scrapper/components/ModalDetail.tsx
+++ client/features/scrapper/components/ModalDetail.tsx
@@ -6,6 +6,8 @@ import {
   ModalHeader,
   Link,
   Image,
+  Accordion,
+  AccordionItem,
 } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
 import { KodanshaResult } from "@/features/scrapper/types";
@@ -25,74 +27,83 @@ export function ModalDetail(props: {
       });
 
       return (
-        <div key={key} className="flex flex-col gap-2 ">
-          <h2 className={"text-xl font-semibold"}>{key}</h2>
-          <div className="grid grid-cols-1 lg:grid-cols-3  gap-2 lg:gap-4">
-            {sortedValue.map((result, index) => (
-              <div key={index} className="flex flex-col gap-2   p-2 rounded-md">
-                <div className="flex gap-4">
-                  <div className={"flex  flex-col gap-2 justify-between"}>
-                    <Link
-                      target={"_blank"}
-                      color="primary"
-                      className="font-semibold"
-                      href={result.readableUrl}
-                    >
-                      {result.relativeName}
-                    </Link>
-                    <Image
-                      src={"/proxy?url=" + result.thumbnail.url}
-                      className={"h-64"}
-                      alt={result.relativeName}
-                    />
-                  </div>
-                  <div className={"flex flex-col justify-center"}>
-                    <div className={"text-sm mb-4"}>
-                      <p>
-                        <span className={"font-semibold"}>Age Rating: </span>
-                        {result.ageRating}
-                      </p>
-                      <p>Full Price: {result.fullPrice}</p>
-                      <p>Discount Price: {result.discountPrice}</p>
-                      <p>
-                        <span className={"font-semibold"}>Discount: </span>
-                        <span
-                          className={
-                            Math.floor(
+        <AccordionItem
+          key={key}
+          aria-label={key}
+          title={`${key} - ${sortedValue.length}`}
+        >
+          <div className="flex flex-col gap-2 ">
+            <div className="grid grid-cols-1 lg:grid-cols-3  gap-2 lg:gap-4">
+              {sortedValue.map((result, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col gap-2   p-2 rounded-md"
+                >
+                  <div className="flex gap-4">
+                    <div className={"flex  flex-col gap-2 justify-between"}>
+                      <Link
+                        target={"_blank"}
+                        color="primary"
+                        className="font-semibold"
+                        href={result.readableUrl}
+                      >
+                        {result.relativeName}
+                      </Link>
+                      <Image
+                        src={"/proxy?url=" + result.thumbnail.url}
+                        className={"h-64"}
+                        alt={result.relativeName}
+                      />
+                    </div>
+                    <div className={"flex flex-col justify-center"}>
+                      <div className={"text-sm mb-4"}>
+                        <p>
+                          <span className={"font-semibold"}>Age Rating: </span>
+                          {result.ageRating}
+                        </p>
+                        <p>Full Price: {result.fullPrice}</p>
+                        <p>Discount Price: {result.discountPrice}</p>
+                        <p>
+                          <span className={"font-semibold"}>Discount: </span>
+                          <span
+                            className={
+                              Math.floor(
+                                100 -
+                                  (result.discountPrice * 100) /
+                                    result.fullPrice,
+                              ) > 40
+                                ? "text-green-600"
+                                : "text-red-600"
+                            }
+                          >
+                            {Math.floor(
                               100 -
                                 (result.discountPrice * 100) / result.fullPrice,
-                            ) > 40
-                              ? "text-green-600"
-                              : "text-red-600"
-                          }
-                        >
-                          {Math.floor(
-                            100 -
-                              (result.discountPrice * 100) / result.fullPrice,
-                          )}
-                          %
-                        </span>
-                      </p>
-                    </div>
+                            )}
+                            %
+                          </span>
+                        </p>
+                      </div>
 
-                    <strong>Links to stores:</strong>
-                    {result.linksToStores.map((link, index) => (
-                      <Link
-                        isExternal
-                        className={"text-blue-500 underline"}
-                        key={index}
-                        target={"_blank"}
-                        href={link.url}
-                      >
-                        {link.name}
-                      </Link>
-                    ))}
+                      <strong>Links to stores:</strong>
+                      {result.linksToStores.map((link, index) => (
+                        <Link
+                          isExternal
+                          className={"text-blue-500 underline"}
+                          key={index}
+                          target={"_blank"}
+                          href={link.url}
+                        >
+                          {link.name}
+                        </Link>
+                      ))}
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
-        </div>
+        </AccordionItem>
       );
     });
   };
@@ -106,7 +117,9 @@ export function ModalDetail(props: {
             <ScrollArea
               className={"h-[calc(100vh-4rem)] w-full rounded-md border p-2"}
             >
-              {props.modalData ? renderModalData() : "No data"}
+              <Accordion selectionMode="multiple">
+                {props.modalData && renderModalData()}
+              </Accordion>
             </ScrollArea>
           </div>
         </ModalBody>
