a7cdf2967e35acaa977b9182cfba9c5a6b4eeff4
Luis <luis.luis@icloud.com>
2025-03-26 15:10:37 -0600

    add auth to ui

--- client/components/PageLayout.tsx
+++ client/components/PageLayout.tsx
@@ -1,5 +1,8 @@
+"use client";
 import { ReactNode } from "react";
 import { cn } from "@/lib/utils";
+import useAuth from "@/features/authentication/hooks/useAuth";
+import Login from "@/features/authentication/components/Login";
 
 const PageLayout = ({
   children,
@@ -10,6 +13,10 @@ const PageLayout = ({
   title: string;
   className?: string;
 }) => {
+  const { user } = useAuth();
+  if (!user) {
+    return <Login />;
+  }
   return (
     <div className={"flex flex-col gap-8  w-full "}>
       <h1 className={"text-4xl font-bold px-12"}>{title}</h1>
